adde4dc2038ad93319feefa582b7206e05ad83ef
Alex Li <lialex0218@gmail.com>
2021-05-24 23:38:28 +0000

    head

--- head.js
+++ head.js
@@ -10,3 +10,20 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+
+const head = function(actual) {
+  if (Array.isArray(actual) === true) {
+    return actual[0];
+  } else if (actual.length === 0) {
+    return undefined;
+  } else {
+    return actual;
+  }
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head([]), 5);
+assertEqual(head("Hello"), "Hello");
