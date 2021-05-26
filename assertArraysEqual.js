2e3005a3117e3d18f104e60c66a7e746c7849e6f
Alex Li <lialex0218@gmail.com>
2021-05-26 04:37:28 +0000

    assert

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function(firstArray, secondArray) {
+  for (let x = 0; x < firstArray.length; x++) {
+    if (firstArray[x] === secondArray[x]) {
+      continue;
+    } else {
+      return (console.log('🛑🛑🛑 Assertion Failed: ',  firstArray, '!==', secondArray,));
+    }
+  }
+  return (console.log('✅✅✅ Assertion Passed: ',  firstArray, '===', secondArray,));
+};
+
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
