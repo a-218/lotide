2a45fb74906ee29477ccd7c299dda722833cfebd
Alex Li <lialex0218@gmail.com>
2021-05-27 04:49:08 +0000

    count only

--- countOnly.js
+++ countOnly.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     return (console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`));
   } else {
@@ -7,39 +7,32 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function(allItems, itemsToCount) {
- 
-  let newObject = {};
-  for (let key in itemsToCount) {
-    if (itemsToCount[key] === true) {
-      newObject[key] = 0;
-    } else {
-      newObject[key] = undefined;
-    }
-  }
 
-  for (let key in newObject) {
-    let count = 0;
-    if (newObject[key] === 0) {
-      for (let x  = 0; x < allItems.length; x++) {
-        if (allItems[x] === key) {
-          count++;
-          console.log(key);
-          console.log(count);
-        }
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    // inside the loop, 
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
       }
-      newObject[key] = count;
     }
-    if (newObject[key] === 0) {
-      newObject[key] = undefined;
-    }
+    console.log(item);
+
   }
-  console.log(newObject);
-  //console.log(itemsToCount.hasOwnProperty(allItems[5]));
-  return newObject;
-};
+  console.log(results);
+
+  return results;
+}
 
 
 
